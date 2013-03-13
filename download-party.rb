5cbc61104bd53be69d8db0a97da4d182a760a72e
Jason Morrison <jason.p.morrison@gmail.com>
2013-03-13 00:13:52 -0700

    csv too

--- download-party.rb
+++ download-party.rb
@@ -9,6 +9,7 @@
 require 'couchrest'
 require 'json'
 require 'time'
+require 'csv'
 require 'pry'
 
 docs = []
@@ -59,27 +60,34 @@ puts "Scans all time: #{scans.count}"
 scans = scans.select { |d| Time.parse(d['created_at']) > Time.parse("March 9, 2013 19:00:00") }
 puts "Scans that night: #{scans.count}"
 
-drinks_headers = %w(rfid_tag_id reader_id timestamp beer_id beer_name tap_name)
 drink_rows = []
-scans.each do |scan_doc|
-  scan_created_at = Time.parse(scan_doc['created_at'])
-  tap_doc = taps.detect { |t| t['reader_id'] == scan_doc['reader_id'] } || next
-  tap_doc['tappings'].each do |tapping_doc|
-    tapping_started_at = Time.parse(tapping_doc['started_at'])
-    tapping_finished_at = Time.parse(tapping_doc['finished_at'])
-
-    if (tapping_started_at < scan_created_at) && (scan_created_at < tapping_finished_at)
-      beer_doc = beers.detect { |b| b['_id'] == tapping_doc['beer_id'] }
-      drink_rows << [
-        scan_doc['tag_id'],
-        scan_doc['reader_id'],
-        scan_doc['created_at'],
-        beer_doc['_id'],
-        beer_doc['name'],
-        tap_doc['name']
-      ]
+drinks_csv = CSV.generate do |csv|
+  csv << %w(rfid_tag_id reader_id timestamp beer_id beer_name tap_name)
+  scans.each do |scan_doc|
+    scan_created_at = Time.parse(scan_doc['created_at'])
+    tap_doc = taps.detect { |t| t['reader_id'] == scan_doc['reader_id'] } || next
+    tap_doc['tappings'].each do |tapping_doc|
+      tapping_started_at = Time.parse(tapping_doc['started_at'])
+      tapping_finished_at = Time.parse(tapping_doc['finished_at'])
+      if (tapping_started_at < scan_created_at) && (scan_created_at < tapping_finished_at)
+        beer_doc = beers.detect { |b| b['_id'] == tapping_doc['beer_id'] }
+        drink_row = [
+          scan_doc['tag_id'],
+          scan_doc['reader_id'],
+          scan_doc['created_at'],
+          beer_doc['_id'],
+          beer_doc['name'],
+          tap_doc['name']
+        ]
+        drink_rows << drink_row
+        csv << drink_row
+      end
     end
   end
+end
+
+open("drinks.csv", "w") do |f|
+  f.puts drinks_csv
 end
 
 rating_scan_headers = %w(reader_id rfid_tag_id timestamp)
@@ -110,18 +118,20 @@ puts "drinking time: #{timespan}"
 
 slice_length = timespan / columns.to_f
 
-beer_popularity.each do |count, name|
-  drinks_per_slice = (0..columns-1).map { |column|
-    slice_start = Time.at(drink_times.first + (slice_length * column))
-    slice_end   = Time.at(slice_start + slice_length)
+open("output1.txt", "w") do |f|
+  beer_popularity.each do |count, name|
+    drinks_per_slice = (0..columns-1).map { |column|
+      slice_start = Time.at(drink_times.first + (slice_length * column))
+      slice_end   = Time.at(slice_start + slice_length)
 
-    drink_rows.select { |d|
-      drank_at = Time.parse(d[2])
-      (d[4] == name) && (slice_start < drank_at) && (drank_at < slice_end)
-    }.count
-  }
+      drink_rows.select { |d|
+        drank_at = Time.parse(d[2])
+        (d[4] == name) && (slice_start < drank_at) && (drank_at < slice_end)
+      }.count
+    }
 
-  spark = `spark #{drinks_per_slice.join(' ')}`
-  header = "#{count} drinks: #{name}"
-  puts "#{header.ljust(50)} #{spark}"
+    spark = `spark #{drinks_per_slice.join(' ')}`
+    header = "#{count} drinks: #{name}"
+    f.puts "#{header.ljust(50)} #{spark}"
+  end
 end

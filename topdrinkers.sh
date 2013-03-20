5b37e5b10a2fcd99c98f96b15336e12dc0c01feb
Jason Morrison <jason.p.morrison@gmail.com>
2013-03-20 10:02:35 -0700

    topdrinkers -> drinkers

--- topdrinkers.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cat drinks.csv| sed 's/,.*//'|sort|uniq -c|sort -r|grep " \\d\{10\}\$" | tee drinkers.txt
-grep "^  1" drinkers.txt | cut -c 6- | xargs -I% sh -c '{ echo ; grep \% drinkers.txt ; grep \% drinks.csv | cut -c 45- | sort; }'

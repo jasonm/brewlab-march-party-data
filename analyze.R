31a6702b022caa2fd0f9681b2a3cb20a4dc54f5b
Jason Morrison <jason.p.morrison@gmail.com>
2013-04-02 20:44:33 -0700

    Sort stacked-line chart (gg.lines) by avg drinking time

--- analyze.R
+++ analyze.R
@@ -5,14 +5,29 @@ require(xts)     || install.packages("xts")
 minutes = 15
 
 DF <- data.frame(read.csv("~/dev/brewlab/party-data/drinks.csv"))
+
+# bucket each drink into a window
 DF$bucket = align.time(as.POSIXct(DF$timestamp), n = 60 * minutes)
+
+# aggregate average drinking time for a beer
+DF$posixct = sapply(DF$timestamp, as.POSIXct)
+aggs = aggregate(DF$posixct, list(BeerName=DF$beer_name),mean)
+names(aggs) = c('beer_name', 'avg_time')
+DF =merge(DF, aggs, by='beer_name')
+
+# factor beer_name levels by average drinking time, for the stacked line chart
+aggs.by.time = aggs[with(aggs, order(avg_time)),]
+DF$beer_name <- factor(DF$beer_name, levels = aggs.by.time$beer_name)
+
 beers.in.buckets = count(DF, vars = c("beer_name","bucket"))
 
 gg.bars <-
   ggplot(data = beers.in.buckets) +
   geom_bar(aes(x=bucket,y=freq,fill=beer_name),stat="identity",position="dodge") +
   xlab(sprintf("%i-minute segments", minutes)) +
-  ylab(sprintf("Pours per beer durint each %i-minute segment", minutes))
+  ylab(sprintf("Pours per beer during each %i-minute segment", minutes))
+
+ggsave(sprintf("gg.bars.%i.pdf", minutes),  plot=gg.bars,  height=8,  width=16)
 
 gg.lines <-
   ggplot(data = beers.in.buckets) +
@@ -20,18 +35,14 @@ gg.lines <-
   facet_wrap(~ beer_name, ncol = 1) +
   xlab(sprintf("Pours per %i minutes", minutes))
 
-ggsave(sprintf("gg.bars.%i.pdf", minutes),  plot=gg.bars,  height=8,  width=16)
 ggsave(sprintf("gg.lines.%i.pdf", minutes), plot=gg.lines, height=48, width=16)
 
-
-# _____________________________________________________________________________________________
-
-# just.pours.in.buckets = count(DF, vars = c("bucket"))
-# gg.overall.lines <-
-#   ggplot(data = just.pours.in.buckets) +
-#   geom_line(aes(x = bucket, y = freq)) +
-#   xlab(sprintf("Pours per %i minutes", minutes))
-# ggsave(sprintf("gg.overall.lines.%i.pdf", minutes), plot=gg.overall.lines,height=8, width=16)
+just.pours.in.buckets = count(DF, vars = c("bucket"))
+gg.overall.lines <-
+  ggplot(data = just.pours.in.buckets) +
+  geom_line(aes(x = bucket, y = freq)) +
+  xlab(sprintf("Pours per %i minutes", minutes))
+ggsave(sprintf("gg.overall.lines.%i.pdf", minutes), plot=gg.overall.lines,height=8, width=16)
 
 #people.in.buckets = count(DF, vars = c("rfid_tag_id", "bucket"))
 #gg.plines <-ggplot(data = people.in.buckets) + geom_line(aes(x = bucket, y = freq)) + facet_wrap(~ rfid_tag_id, ncol = 1) + theme(strip.background = element_blank(), strip.text.x = element_blank(), strip.text.y = element_blank())
